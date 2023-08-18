f67aa6614eccb783ed2f9bf200f21100376b86ae
sire <robertcxlee@gmail.com>
2023-08-18 17:32:11 -0600

    added pause logic

--- src/game.cpp
+++ src/game.cpp
@@ -1,15 +1,61 @@
 #include "./piece.cpp"
+#include <GL/glad.h>
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
 #include <Windows.h>
+#include <iostream>
 
 class Game {
+  private:
+	bool bgmEnabled = true;
+	bool escDown = false;
+
   public:
 	Game() {}
 
+	// AUDIO
 	void initAudio() {
 		mciSendStringA(
 			"open \"D:\\robert2021\\projects\\tetrisGL\\src\\assets\\bgm.mp3\" "
 			"type mpegvideo alias bgm",
 			NULL, 0, NULL);
 		mciSendStringA("play bgm repeat", NULL, 0, NULL);
+	}
+
+	void pauseAudio() {
+		if (bgmEnabled) {
+			mciSendStringA("pause bgm", NULL, 0, NULL);
+		} else {
+			mciSendStringA("resume bgm", NULL, 0, NULL);
+		}
+		bgmEnabled = !bgmEnabled;
+	}
+
+	// INPUT
+	void getInput(GLFWwindow *window) {
+
+		// ESC: pause game
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			escDown = true;
+		}
+		if (escDown && glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_RELEASE) {
+			escDown = false;
+			pauseAudio();
+		}
+	}
+
+	// RENDERING
+	void render() {
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
+							0.0f,  0.0f,  0.5f, 0.0f};
+
+		unsigned int VBO;
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+					 GL_STATIC_DRAW);
 	}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,40 @@
 #include "./game.cpp"
 #include "./home.cpp"
+#include <GL/glad.h>
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
+#include <Windows.h>
 #include <iostream>
 #include <string>
+#include <thread>
 
 int main(void) {
 
 	// init window
 	glfwInit();
 	GLFWwindow *window;
-	window = glfwCreateWindow(640, 480, "TetrisGL", NULL, NULL);
+	window = glfwCreateWindow(400, 800, "TetrisGL", NULL, NULL);
 	glfwMakeContextCurrent(window);
 
+	// init openGL
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	glViewport(0, 0, 400, 800);
+
+	// init game
 	Game *game = new Game();
-	// game->initAudio();
+	game->initAudio();
 
 	// main game loop
 	while (!glfwWindowShouldClose(window)) {
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
 
-		/* Swap front and back buffers */
+		// input
+		game->getInput(window);
+
+		// rendering
+		game->render();
+
+		// updates window
 		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
